dca766b3a4c5830cc03985ce164aa0a0f1187cc7
Thomas Petersson <msdn@thomaspetersson.se>
2022-12-01 00:01:13 +0100

    First commit

---
+++ Day01/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("01 dec, Hello, World!");
+
+var input1 = File.ReadAllLines("input1.txt");
+
+
+
+//var input2 = File.ReadAllLines("input2.txt");
