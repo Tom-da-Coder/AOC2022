7ce155e894c3d8b191de38199e091182fc09b009
Thomas Petersson <msdn@thomaspetersson.se>
2022-12-01 23:29:11 +0100

    Day 1 finished

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,8 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+using System.Net.Sockets;
+
+int day = 1;
 Console.WriteLine("01 dec, Hello, World!");
 
 var input1 = File.ReadAllLines("input1.txt");
+var elves = new List<int>();
 
-
+int sum = 0;
+foreach (var elve in input1)
+{
+    if (elve.Length == 0)
+    {
+        elves.Add(sum);
+        sum = 0;
+    }
+    else
+    {
+        sum += int.Parse(elve);
+    }
+}
+Console.WriteLine(elves.Max());
+Console.WriteLine(elves.OrderByDescending(c => c).Take(3).Sum());
 
 //var input2 = File.ReadAllLines("input2.txt");
