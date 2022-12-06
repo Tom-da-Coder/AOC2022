9fb2f20919f8791316272532eed51c56979ab861
Thomas Petersson <msdn@thomaspetersson.se>
2022-12-06 23:14:07 +0100

    Day 6 finished

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.ExceptionServices;
+
 Console.WriteLine("06 dec, Hello, World!");
 
-int result = 0;
-var input1 = File.ReadAllLines("input1.txt");
+var inp = File.ReadAllText("input1.txt");
 
 
 
@@ -15,19 +16,41 @@ Console.WriteLine(partB());
 int partA()
 {
     int result = 0;
-    foreach (var line in input1)
+    for (int i = 4; i < inp.Length; i++)
     {
+        if (check(inp[i], inp[i - 1], inp[i - 2], inp[i - 3]))
+            return i + 1;
     }
-    return result;
+    return -1;
+
+    bool check(char A, char B, char C, char D)
+    {
+        return A != B && A != C && A != D && B != C && B != D && C != D;
+    }
 }
 
 
 
 int partB()
 {
-    int result = 0;
-    foreach (var line in input1)
+    int n = 14;
+    for (int i = n; i < inp.Length; i++)
     {
+        if (test(i))
+            return i + 1;
     }
-    return result;
+    return -1;
+
+    bool test(int i)
+    {
+        var bin = new bool[26];
+        for (int k = i - n + 1; k <= i; k++)
+        {
+            var c = inp[k] - 'a';
+            if (bin[c])
+                return false;
+            bin[c] = true;
+        }
+        return true;
+    }
 }
