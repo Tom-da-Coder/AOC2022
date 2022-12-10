ed7c689c498d0dccb2d031829930ed03c00b2254
Thomas Petersson <msdn@thomaspetersson.se>
2022-12-10 02:26:57 +0100

    Förberett till 14:e

---
+++ Day12/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("12 dec, Hello, World!");
+
+int result = 0;
+var input1 = File.ReadAllLines("input1.txt");
+
+
+
+//var input2 = File.ReadAllLines("input2.txt");
+
+Console.WriteLine(partA());
+Console.WriteLine(partB());
+
+int partA()
+{
+    int result = 0;
+    foreach (var line in input1)
+    {
+    }
+    return result;
+}
+
+
+
+int partB()
+{
+    int result = 0;
+    foreach (var line in input1)
+    {
+    }
+    return result;
+}
