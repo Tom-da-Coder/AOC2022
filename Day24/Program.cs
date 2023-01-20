ab49b864b9038f7952b0dc334ec47cc7b622dd1e
Thomas Petersson <msdn@thomaspetersson.se>
2023-01-21 00:38:47 +0100

    Börjat felsöka dag 24

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -1,126 +1,117 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Data;
-using System.Reflection;
-using System.Reflection.Metadata;
-
-Console.WriteLine("24 dec, Hello, World!");
-
-var ResultsList = new List<int>();
-int minResult = int.MaxValue;
-var input1 = File.ReadAllLines("input1.txt");
-var blizzards = new List<Blizzard>();
-Blizzard.Height = input1.Length - 2;
-Blizzard.Width = input1[0].Length - 2;
-for (int r = 0; r < input1.Length; r++)
+﻿internal class Program
 {
-    string line = input1[r];
-    for (int c = 0; c < line.Length; c++)
+    private static void Main(string[] args)
     {
-        var ch = line[c];
-        var ix = ">v<^".IndexOf(line[c]);
-        if (ix >= 0)
-            blizzards.Add(new Blizzard { row = r - 1, col = c - 1, dir = (Blizzard.Direction)ix });
-    }
-}
+        Console.WriteLine("24 dec, Hello, World!");
 
-partA();
-Console.WriteLine(ResultsList.Min());
+        var ResultsList = new List<int>();
+        var input1 = File.ReadAllLines("input1.txt");
+        int Height = input1.Length - 2;
+        int Width = input1[0].Length - 2;
+        int[,,] DistMap = new int[1000, Height, Width];
+        var ActivePoses = new List<Track>();
+        var result = 0;
 
-//Console.WriteLine(partB());
+        prepDistMap();
 
-int partA()
-{
-    RecursiveMove(1, 0, 0);
-    return 0;
-}
+        partA();
 
-
-
-int partB()
-{
-    int result = 0;
-    foreach (var line in input1)
-    {
-    }
-    return result;
-}
-
-void RecursiveMove(int gen, int r, int c)
-{
-    if (gen > minResult)
-        return;
-    if (r == input1.Length - 3 && c == input1[0].Length - 3)
-    {
-        ResultsList.Add(gen + 1);
-        if (minResult > gen + 1)
-            minResult= gen + 1;
-        return;
-        //Console.WriteLine(gen + 1);
-        //throw new FinishedException();
-    }
-
-    if (CheckNextPosition(r, c + 1, gen + 1)) RecursiveMove(gen + 1, r, c + 1);
-    if (CheckNextPosition(r + 1, c, gen + 1)) RecursiveMove(gen + 1, r + 1, c);
-    if (CheckNextPosition(r, c - 1, gen + 1)) RecursiveMove(gen + 1, r, c - 1);
-    if (CheckNextPosition(r - 1, c, gen + 1)) RecursiveMove(gen + 1, r - 1, c);
-    if (CheckNextPosition(r, c, gen + 1)) RecursiveMove(gen + 1, r, c);
-}
-
-// Return true when free
-//bool CheckNextPosition(int r, int c, int gen) {
-//    return InRange(r, c) && blizzards.Where(b => b.row == r).All(w => w.IsWhere(gen).col != c) &&
-//    blizzards.Where(b => b.col == c).All(w => w.IsWhere(gen).row != r);
-//}
-
-bool CheckNextPosition(int r, int c, int gen)
-{
-    if (!InRange(r, c)) return false;
-    if (input1[r + 1][LimitCol(c + gen) + 1] == '<') return false;
-    if (input1[LimitRow(r + gen) + 1][c + 1] == '^') return false;
-    if (input1[r + 1][LimitCol(c - gen) + 1] == '>') return false;
-    if (input1[LimitRow(r - gen) + 1][c + 1] == 'v') return false;
-    return true;
-}
-
-
-int LimitRow(int r)
-{
-    while (r < 0) r += Blizzard.Height;
-    r %= Blizzard.Height;
-    return r;
-}
-
-int LimitCol(int c)
-{
-    while (c < 0) c += Blizzard.Width;
-    c %= Blizzard.Width;
-    return c;
-}
-
-bool InRange(int r, int c) => r >= 0 && r < Blizzard.Height && c >= 0 && c < Blizzard.Width;
-
-public class Blizzard
-{
-    public static int Height, Width;
-
-    public int row;
-    public int col;
-    public enum Direction { East, South, West, North };
-    public Direction dir;
-
-    public (int row, int col) IsWhere(int atGen)
-    {
-        switch (dir)
+        void prepDistMap()
         {
-            case Direction.South: return Limit(row + atGen, col);
-            case Direction.North: return Limit(row - atGen, col);
-            case Direction.East: return Limit(row, col + atGen);
-            case Direction.West: return Limit(row, col - atGen);
+            Array.Clear(DistMap);
         }
-        return (-1, -1);
 
-        (int r, int c) Limit(int r, int c) => ((r + 4 * Height) % Height, (c + 4 * Width) % Width);
+        int partA()
+        {
+            RunOnce(false, 1);
+            var startStep = result + 1;
+            result = 0;
+            while (result == 0)
+            {
+                RunOnce(true, startStep);
+                startStep++;
+            }
+            startStep = 568; // result + 1;
+            result = 0;
+            while (result == 0)
+            {
+                RunOnce(false, startStep);
+                startStep++;
+            }
+            return 0;
+        }
+
+        void RunOnce(bool back, int start)
+        {
+            ActivePoses.Clear();
+            ActivePoses.Add(new Track { R = back ? Height - 1 : 0, C = back ? Width - 1 : 0, Step = start });
+            while (ActivePoses.Count > 0)
+            {
+                var next = ActivePoses.MinBy(p => p.Step);
+                ActivePoses.Remove(next);
+                if (CheckMoves(next, back))
+                    break;
+            }
+        }
+
+        bool CheckMoves(Track trk, bool returning)
+        {
+            (int gen, int r, int c) = (trk.Step, trk.R, trk.C);
+            if (!returning && r == Height - 1 && c == Width - 1 || returning && r == 0 && c == 0)
+            {
+                if (returning) PrintTrack(trk);
+                result = gen + 1;
+                Console.WriteLine(gen + 1);
+                return true;
+            }
+
+            CheckNextPosition(r, c + 1, gen + 1, trk);
+            CheckNextPosition(r + 1, c, gen + 1, trk);
+            CheckNextPosition(r, c - 1, gen + 1, trk);
+            CheckNextPosition(r - 1, c, gen + 1, trk);
+            CheckNextPosition(r, c, gen + 1, trk);
+            return false;
+        }
+
+        bool CheckNextPosition(int r, int c, int gen, Track? from = null)
+        {
+            if (!InRange(r, c)) return false;
+            if (input1[r + 1][WrapCol(c + gen) + 1] == '<') return false;
+            if (input1[WrapRow(r + gen) + 1][c + 1] == '^') return false;
+            if (input1[r + 1][WrapCol(c - gen) + 1] == '>') return false;
+            if (input1[WrapRow(r - gen) + 1][c + 1] == 'v') return false;
+            if (ActivePoses.All(t => t.Step != gen || t.R != r || t.C != c))
+                ActivePoses.Add(new Track { Step = gen, R = r, C = c, ComesFrom = from });
+            return true;
+        }
+
+        void PrintTrack(Track? track)
+        {
+            var steps = new List<Track>();
+            while (track != null)
+            {
+                steps.Add(track);
+                track = track.ComesFrom;
+            }
+            steps.Reverse();
+            Console.WriteLine(string.Join(Environment.NewLine, steps.Select(s => $"{s.Step}, {s.R}, {s.C}")));
+        }
+
+
+        int WrapRow(int r) => Wrap(Height, r);
+
+        int WrapCol(int c) => Wrap(Width, c);
+
+        int Wrap(int range, int val) => (val % range < 0 ? range : 0) + val % range;
+
+        bool InRange(int r, int c) => r >= 0 && r < Height && c >= 0 && c < Width;
     }
 }
 
-public class FinishedException : Exception { }
+class Track
+{
+    public int R;
+    public int C;
+    public int Step;
+    public Track? ComesFrom;
+}
