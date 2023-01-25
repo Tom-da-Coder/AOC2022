55b4f2e68c1a24d8ef8181b5d1da68caf5ca748d
Thomas Petersson <msdn@thomaspetersson.se>
2023-01-25 23:00:25 +0100

    Rensad och fungerar

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -1,136 +1,76 @@
-﻿using System.Runtime.Serialization.Formatters.Binary;
-
-internal class Program
+﻿internal class Program
 {
-    public static int[,,] TPmaze = new int[301, 27, 122];
-
     private static void Main(string[] args)
     {
         Console.WriteLine("24 dec, Hello, World!");
 
-        var ResultsList = new List<int>();
         var input1 = File.ReadAllLines("input1.txt");
         int Height = input1.Length - 2;
         int Width = input1[0].Length - 2;
-        int[,,] DistMap = new int[1000, Height, Width];
+        (int r, int c) StartPnt = (-1, 0);
+        (int r, int c) EndPnt = (Height, Width - 1);
+        (int r, int c) StopAt;
         var ActivePoses = new List<Track>();
         var result = 0;
-        //prepDistMap();
-        //LoadArray();
-        //FindPath();
 
-        partA();
+        RunOnce(0, StartPnt, EndPnt);
+        var startStep = result + 1;
+        result = 0;
+        RunOnce(startStep, EndPnt, StartPnt);
+        startStep = result + 1;
+        result = 0;
+        RunOnce(startStep, StartPnt, EndPnt);
 
-        void prepDistMap()
+        void RunOnce(int startStep, (int r, int c) startPos, (int, int) endPos)
         {
-            Array.Clear(DistMap);
-        }
-
-        int partA()
-        {
-            RunOnce(false, 1);
-            var startStep = result + 1;
-            result = 0;
-            while (result == 0)
-            {
-                RunOnce(true, startStep);
-                startStep++;
-            }
-            startStep = 568; // result + 1;
-            result = 0;
-            while (result == 0)
-            {
-                RunOnce(false, startStep);
-                startStep++;
-            }
-            return 0;
-        }
-
-        void RunOnce(bool back, int start)
-        {
+            StopAt = endPos;
             ActivePoses.Clear();
-            int r = back ? Height - 1 : 0, c = back ? Width - 1 : 0;
-            while (!CheckNextPosition(r, c, start))
-                start++;
-            ActivePoses.Add(new Track { R = r, C = c, Step = start });
-            while (ActivePoses.Count > 0)
+            //int r = back ? Height : -1, c = back ? Width - 1 : 0;
+            do
             {
-                var next = ActivePoses.MinBy(p => p.Step);
-                ActivePoses.Remove(next);
-                if (CheckMoves(next, back))
-                    break;
-            }
+                ActivePoses.Add(new Track { R = startPos.r, C = startPos.c, Step = startStep });
+                while (ActivePoses.Count > 0)
+                {
+                    var next = ActivePoses.MinBy(p => p.Step);
+                    ActivePoses.Remove(next);
+                    CheckMoves(next, endPos);
+                    if (result != 0)
+                        break;
+                }
+                startStep++;
+            } while (result == 0);
+            Console.WriteLine(result);
         }
 
-        bool CheckMoves(Track trk, bool returning)
+        bool CheckMoves(Track trk, (int, int) endPos)
         {
             (int gen, int r, int c) = (trk.Step, trk.R, trk.C);
-            if (!returning && r == Height - 1 && c == Width - 1 || returning && r == 0 && c == 0)
-            {
-                if (returning) PrintTrack(trk);
-                result = gen + 1;
-                Console.WriteLine(gen + 1);
-                return true;
-            }
-
-            CheckNextPosition(r, c + 1, gen + 1, trk);
-            CheckNextPosition(r + 1, c, gen + 1, trk);
-            CheckNextPosition(r, c - 1, gen + 1, trk);
-            CheckNextPosition(r - 1, c, gen + 1, trk);
-            CheckNextPosition(r, c, gen + 1, trk);
+            //if ((r, c) == endPos)
+            //{
+            //    result = gen + 1;
+            //    Console.WriteLine(gen + 1);
+            //    return true;
+            //}
+            
+            CheckNextPosition(r, c + 1, gen + 1);
+            CheckNextPosition(r + 1, c, gen + 1);
+            CheckNextPosition(r, c - 1, gen + 1);
+            CheckNextPosition(r - 1, c, gen + 1);
+            CheckNextPosition(r, c, gen + 1);
             return false;
         }
 
-        bool CheckNextPosition(int r, int c, int gen, Track? from = null)
+        void CheckNextPosition(int r, int c, int gen)
         {
-            if (!InRange(r, c)) return false;
-            if (input1[r + 1][WrapCol(c + gen) + 1] == '<') return false;
-            if (input1[WrapRow(r + gen) + 1][c + 1] == '^') return false;
-            if (input1[r + 1][WrapCol(c - gen) + 1] == '>') return false;
-            if (input1[WrapRow(r - gen) + 1][c + 1] == 'v') return false;
+            if ((r, c) == StopAt) { result = gen; return; }
+            if (!InRange(r, c)) return;
+            if (input1[r + 1][WrapCol(c + gen) + 1] == '<') return;
+            if (input1[WrapRow(r + gen) + 1][c + 1] == '^') return;
+            if (input1[r + 1][WrapCol(c - gen) + 1] == '>') return;
+            if (input1[WrapRow(r - gen) + 1][c + 1] == 'v') return;
             if (ActivePoses.All(t => t.Step != gen || t.R != r || t.C != c))
-                ActivePoses.Add(new Track { Step = gen, R = r, C = c, ComesFrom = from });
-            return true;
-        }
-
-        void PrintTrack(Track? track)
-        {
-            var steps = new List<Track>();
-            while (track != null)
-            {
-                steps.Add(track);
-                track = track.ComesFrom;
-            }
-            steps.Reverse();
-            Console.WriteLine(string.Join(Environment.NewLine, steps.Select(s => $"{s.Step}, {s.R}, {s.C}")));
-        }
-
-        void SaveArray()
-        {
-            using (var strm = new FileStream("fil.bin", FileMode.Create))
-                
-            {
-                var fmt = new BinaryFormatter();
-                fmt.Serialize(strm, DistMap);
-                strm.Flush();
-            }
-        }
-
-        void LoadArray()
-        {
-            using (var strm = new FileStream("TPdump.bin", FileMode.Open, FileAccess.Read))
-            {
-                var fmt = new BinaryFormatter();
-                TPmaze = (int[,,])fmt.Deserialize(strm);
-            }
-        }
-
-        void FindPath()
-        {
-            int r = 26, c = 120;
-            int g = 0;
-            while (TPmaze[g, r, c] == 0)
-                g++;
+                ActivePoses.Add(new Track { Step = gen, R = r, C = c });
+            return;
         }
 
         int WrapRow(int r) => Wrap(Height, r);
@@ -148,5 +88,4 @@ class Track
     public int R;
     public int C;
     public int Step;
-    public Track? ComesFrom;
 }
